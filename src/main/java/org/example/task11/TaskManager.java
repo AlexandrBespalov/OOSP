69bbe2402b8b22d6893a8ac28281b77136b59100
AlexandrBespalov <bespalov0710@gmail.com>
2024-11-13 19:18:16 +0300

    Add TaskManager at the 11th task

--- src/main/java/org/example/task11/TaskManager.java
+++ src/main/java/org/example/task11/TaskManager.java
@@ -1,4 +1,26 @@
 package org.example.task11;
 
 public class TaskManager {
+    public static void main(String[] args) {
+        // Создание исходных задач
+        Task devTask = new DevelopmentTask("Разработка новой фичи", "Добавить функцию фильтрации", "Высокий");
+        Task testTask = new TestingTask("Тестирование новой фичи", "Провести тестирование фильтрации", "Средний");
+
+        System.out.println("Исходные задачи:");
+        System.out.println(devTask);
+        System.out.println(testTask);
+
+        // Клонирование задач
+        Task clonedDevTask = devTask.clone();
+        Task clonedTestTask = testTask.clone();
+
+        // Изменяем параметры клонированных задач
+        clonedDevTask.setTitle("Разработка новой фичи (клонированная)");
+        clonedTestTask.setPriority("Низкий");
+
+        // Выводим клонированные задачи
+        System.out.println("\nКлонированные задачи:");
+        System.out.println(clonedDevTask);
+        System.out.println(clonedTestTask);
+    }
 }
